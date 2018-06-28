0eab152d3b5cdfae5f184e5b4198609f5d264fca
jagathshreeiyer <JagathshreeIyer@carfax.com>
2018-06-28 14:16:27 -0400

    Generating new deck in a fancy way instead of hardcoding

--- Cards/deck.go
+++ Cards/deck.go
@@ -4,6 +4,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for index, deck := range d {
 		fmt.Println(index, deck)
