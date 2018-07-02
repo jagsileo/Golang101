a12f4bf6ae46798fc5b27f6a845c7046c31b8b5b
jagathshreeiyer <JagathshreeIyer@carfax.com>
2018-07-02 15:18:38 -0400

    Deck shuffling

--- Cards/main.go
+++ Cards/main.go
@@ -2,5 +2,14 @@ package main
 
 func main() {
 	cards := newDeck()
+	//cards.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// fmt.Println("<<<<Hand>>>>>>")
+	// hand.print()
+	// fmt.Println("<<<<Remaining Deck>>>>")
+	// remainingDeck.print()
+	// cards.saveToFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
