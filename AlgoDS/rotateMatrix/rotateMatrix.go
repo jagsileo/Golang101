04a27c84c5f4f337746484b32740006b29447cea
Jagathshree <jagathshree@Jagathshrees-MacBook-Air.local>
2019-04-12 07:12:40 -0400

    Broken rotate matrix, saving work

---
+++ AlgoDS/rotateMatrix/rotateMatrix.go
@@ -0,0 +1,24 @@
+package rotateMatrix
+
+func rotateMatrix(matrix [][]int) [][]int {
+	rowCount := len(matrix)
+
+	colCount := len(matrix[0])
+
+	if rowCount != colCount {
+		return [][]int{}
+	}
+
+	for i := 0; i <= rowCount/2; i++ {
+		for j := i; j <= colCount/2; j++ {
+			buf := matrix[i][j]
+			matrix[i][j] = matrix[rowCount-1-j][i]
+			matrix[rowCount-1-j][i] = matrix[rowCount-1-i][rowCount-1-j]
+			matrix[rowCount-1-i][rowCount-1-j] = matrix[j][rowCount-1-i]
+			matrix[j][rowCount-1-i] = buf
+
+		}
+	}
+
+	return matrix
+}
---
+++ AlgoDS/rotateMatrix/rotateMatrix_test.go
@@ -0,0 +1,26 @@
+package rotateMatrix
+
+import (
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestRotateMatrix(t *testing.T) {
+
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+	newMatrix := rotateMatrix(matrix)
+	log.Println(newMatrix)
+	if !reflect.DeepEqual(newMatrix, [][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}) {
+		t.Error("Test Failed for 3x3")
+	}
+
+	matrix = [][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}
+
+	newMatrix = rotateMatrix(matrix)
+	log.Println(newMatrix)
+	if !reflect.DeepEqual(newMatrix, [][]int{{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}}) {
+		t.Error("Test Failed for 4x4")
+	}
+}
